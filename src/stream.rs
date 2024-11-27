4fdea49796fc3e75032c6402a3edb3023ac3334f
wild <wildwildwildthings@gmail.com>
2024-11-27 20:04:57 +0700

    stream impl

--- src/stream.rs
+++ src/stream.rs
@@ -1,12 +1,11 @@
-use crate::parser::{Field, FieldKind, ParseResult};
+use crate::parser::{Field, FieldKind, ParseResult, ParsedLine};
 use crate::Error;
 use crate::{parser, Event};
 use futures_core::Stream;
+use memchr::memchr;
 use pin_project_lite::pin_project;
 use std::pin::Pin;
-use std::string::FromUtf8Error;
-use std::task::{Context, Poll};
-use memchr::memchr;
+use std::task::{ready, Context, Poll};
 
 struct EventBuilder {
     event: Event
@@ -65,11 +64,62 @@ impl EventBuilder {
     }
 }
 
+/// Attempts to parse the lines provided in a buffer for a complete event.
+///
+/// Might return an event, otherwise will return when it encounters an incomplete line.
+/// Also returns the remaining buffer bytes.
+fn parse_lines_for_event<'a, 'b, E>(
+    mut buffer: &'a [u8],
+    builder: &'b mut EventBuilder
+) -> Result<(Option<Event>, &'a [u8]), Error<E>> {
+    loop {
+        let res = parser::parse_line(buffer);
+
+        match res {
+            ParseResult::Parsed { line, rem } => {
+                buffer = rem;
+
+                match line {
+                    ParsedLine::Field(field) => {
+                        builder.process_field(field)?;
+                    }
+                    ParsedLine::Dispatch => {
+                        break Ok((
+                            builder.dispatch(),
+                            rem
+                        ))
+                    }
+                    ParsedLine::Ignored => {}
+                }
+            }
+            ParseResult::Incomplete => break Ok((
+                None,
+                buffer
+            ))
+        }
+    }
+}
+
+fn strip_bom<'a, 'b>(bytes: &'a [u8], bom_checked: &'b mut bool) -> &'a [u8] {
+    const UTF_8_BOM: &[u8] = b"\xEE\xBB\xBF";
+
+    if !*bom_checked && bytes.len() >= UTF_8_BOM.len() {
+        *bom_checked = true;
+        bytes.strip_prefix(UTF_8_BOM).unwrap_or_else(|| bytes)
+    } else {
+        bytes
+    }
+}
+
+
 // todo handle bom
 pin_project! {
     pub struct EventStream<St> {
+        #[pin]
         stream: St,
-        buffer: Vec<u8>
+        buffer: Vec<u8>,
+        builder: EventBuilder,
+        bom_checked: bool
     }
 }
 
@@ -83,6 +133,49 @@ where
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
 
-        todo!()
+        // The remaining buffer could still have complete events. We should process them.
+        if let (Some(event), rem) = parse_lines_for_event(&this.buffer, &mut this.builder)? {
+            this.buffer.drain(..this.buffer.len() - rem.len());
+            return Poll::Ready(Some(Ok(event)))
+        }
+
+        // Otherwise, poll the stream for new bytes.
+        // If the buffer is empty, process for an event. Return an event if found. Rest should be buffered.
+        // (we should also check for a BOM here)
+        // If the buffer is NOT empty, buffer all the new bytes. Then, process for an event.
+
+        Poll::Ready(loop {
+            let Some(bytes) = ready!(this.stream.as_mut().poll_next(cx)) else {
+                // Stream ended
+                break None
+            };
+
+            let bytes = bytes.map_err(Error::Source)?;
+
+            let event = if this.buffer.is_empty() {
+                let bytes = strip_bom(bytes.as_ref(), &mut this.bom_checked);
+
+                let (event, rem) = parse_lines_for_event(bytes, &mut this.builder)?;
+
+                this.buffer.extend_from_slice(rem);
+
+                event
+            } else {
+                this.buffer.extend_from_slice(bytes.as_ref());
+
+                let bytes = strip_bom(bytes.as_ref(), &mut this.bom_checked);
+
+                let (event, rem) = parse_lines_for_event(bytes, &mut this.builder)?;
+
+                this.buffer.drain(..this.buffer.len() - rem.len());
+
+                event
+            };
+
+            match event {
+                None => continue,
+                Some(event) => break Some(Ok(event))
+            }
+        })
     }
 }
